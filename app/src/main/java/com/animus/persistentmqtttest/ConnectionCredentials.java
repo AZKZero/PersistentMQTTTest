9d2d55a67e050bed6e5e8e230e834940a2f016ad
Abdullah Zowad Khan <khanabdullahzowad@gmail.com>
2019-10-23 21:28:47 +0600

    titan 1

--- app/src/main/java/com/animus/persistentmqtttest/ConnectionCredentials.java
+++ app/src/main/java/com/animus/persistentmqtttest/ConnectionCredentials.java
@@ -3,6 +3,6 @@ package com.animus.persistentmqtttest;
 public class ConnectionCredentials {
 
     public static final String BROKER_URL = "tcp://www.YOURSITE.com:1883";
-    public static final String USER = "rams";
-    public static final String PASSWORD = "propyl-fear-plot";
+    public static final String USER = "YOUR_USERNAME";
+    public static final String PASSWORD = "YOUR_PASSWORD";
 }
